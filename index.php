a8ed4d1e2031b15fff1e5e5b1036d2f793e3bbfa
cetecom <cetecom@LC08L1528>
2019-04-08 21:01:41 -0300

    Ejercicio 2 con Iconos

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ and open the template in the editor.
         <title>HOSTALRIOAMAZONAS RESERVATION ONLINE</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         <link href="css/estiloprincipal.css" rel="stylesheet" type="text/css"/>
--- menu.php
+++ menu.php
@@ -2,6 +2,8 @@
 <a class="list-group-item" href="<?=URLBASE;?>"><i class="fa fa-home fa-2x"></i> Home</a>
 <a id="prisolicitud" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Acceda a las solicitudes"><i class="fa fa-file-o fa-2x"></i> Solicitudes</a>
 <a id="subsolicitud" class="list-group-item submenu" href="<?=URLBASE;?>contenido/formvacaciones.php" ><i class="fa fa-sun-o fa-2x"></i> Ingreso Vacaciones</a>
+<a id="subsolicitud" class="list-group-item submenu" href="<?=URLBASE;?>contenido/Reservation.php" ><i class="fas fa-hotel fa-2x"></i></i> Reservation</a>
+<a id="subsolicitud" class="list-group-item submenu" href="<?=URLBASE;?>contenido/AboutChile.php" ><i class="fas fa-hotel fa-2x"></i></i> Reservation</a>
 <a id="subsolicitud-lista" class="list-group-item submenu" href="<?=URLBASE;?>contenido/formlistavacaciones.php" ><i class="fa fa-sun-o fa-2x"></i> Ver Vacaciones Solicitadas</a>
 <a class="list-group-item" href="#"><i class="fa fa-birthday-cake fa-2x"></i> Cumpleaños</a>
 <a class="list-group-item" href="#"><i class="fa fa-file-o fa-2x"></i> Certificados</a>
